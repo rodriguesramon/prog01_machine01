751bdcf83ca46c67d5865e78f85809c9f3003fdd
System Administrator <root@MacBook-Pro-de-Aluno.local>
2018-10-19 18:49:15 -0400

    Commit 01

---
+++ Prog01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
